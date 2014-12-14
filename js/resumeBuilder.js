f854d68bb9145a69de086c3f4183ca1933ad42a9
Joshua Briley <josh@thebrileys.com>
2014-12-14 12:52:41 -0500

    Initial commit.

---
+++ js/resumeBuilder.js
@@ -0,0 +1,139 @@
+var bio = {
+  "name" : "Joshua Briley",
+  "role" : "Front End Developer",
+  "contactInfo" : {
+    "streetAddress" : "56 Starkweather St.",
+    "city" : "Manchester",
+    "state" : "CT",
+    "zip" : "06042",
+    "email" : "josh@thebrileys.com",
+    "phone" : "8602328250",
+    "github" : "somecallmejosh",
+    "twitter" : "joshuabriley"
+  },
+  "skills" : [
+    "Front End Developer", "UI Designer", "Musician"
+  ],
+  "bioPic" : "http://lorempixel.com/400/400/people/",
+  "welcomeMessage" : "Hi, I'm Josh. Yadda Yadda Yadda."
+};
+
+var work = {
+  "jobs" :
+    [
+      {
+        "employer" : "Sports Technologies, LLC",
+        "title" : "Front End Developer",
+        "location" : "Collinsville, CT",
+        "dates" : "June 13 - Present",
+        "description" : "Front End Design and Development of Fantasy Sports Applications."
+      },
+      {
+        "employer" : "East Point Systems, Inc",
+        "title" : "Front End Developer",
+        "location" : "East Hartford, CT",
+        "dates" : "Jan 13 - May 13",
+        "description" : "Front End Design and Development of Online Housing Reposession Industry Network."
+      },
+      {
+        "employer" : "ImageWorks, LLC",
+        "title" : "Lead Designer and Front End Developer",
+        "location" : "Vernon, CT",
+        "dates" : "Aug 08 - Jan 13",
+        "description" : "Front End Design and Development of conversion oriented marketing websites."
+      },
+      {
+        "employer" : "Para-Diddle Design, LLC",
+        "title" : "Owner and Front End Developer",
+        "location" : "Manchester, CT",
+        "dates" : "Jan 04 - Aug 08",
+        "description" : "Front End Design and Development of conversion oriented marketing websites."
+      }
+    ]
+};
+
+var projects = {
+  "projects" : [
+    {
+      "title" : "Kenwood Dennard",
+      "dates" : "Aug 2014",
+      "description" : "AngularFire website that allows easy direct editing of page contents.",
+      "images" : [
+        "https://dl.dropboxusercontent.com/u/39806773/josh-projects/kenwood-dennard.png",
+        "https://dl.dropboxusercontent.com/u/39806773/josh-projects/kenwood-editable.png"
+      ]
+    },
+    {
+      "title" : "Chase The Kicker",
+      "dates" : "Jul 2013 - May 2014",
+      "description" : "College recruiting website that displays seasonal statistics and video highlights.",
+      "images" : [
+        "https://dl.dropboxusercontent.com/u/39806773/josh-projects/chase-the-kicker.png",
+        "https://dl.dropboxusercontent.com/u/39806773/josh-projects/chase-the-kicker-bio.png"
+      ]
+    },
+    {
+      "title" : "CT Drummers Association",
+      "dates" : "Jan 2012 - Present",
+      "description" : "Fundraising website to earn scholarship money for college bound drummers.",
+      "images" : [
+        "https://dl.dropboxusercontent.com/u/39806773/josh-projects/ct-crummers.png"
+      ]
+    }
+  ]
+};
+
+var education = {
+  "schools" : [
+    {
+      "name" : "Troy University",
+      "city" : "Troy, AL",
+      "degree" : "Bachelors",
+      "graduationYear" : 2001,
+      "majors" : ["Biology"]
+    },
+    {
+      "name" : "City Colleges of Chicago",
+      "city" : "Hohenfels, Germany",
+      "degree" : "Associates",
+      "graduationYear" : 1997,
+      "majors" : ["General Studies"]
+    }
+  ],
+  "onlineCourses" : [
+    {
+      "title" : "Intro To Computer Science",
+      "school" : "Udacity",
+      "date" : 2014,
+      "url" : "https://www.udacity.com/course/cs101"
+    },
+    {
+      "title" : "Javascript Basics",
+      "school" : "Udacity",
+      "date" : 2014,
+      "url" : "https://www.udacity.com/course/ud804"
+    },
+    {
+      "title" : "Javascript",
+      "school" : "Codecademy",
+      "date" : 2012,
+      "url" : "http://www.codecademy.com/en/tracks/javascript"
+    },
+    {
+      "title" : "JQuery",
+      "school" : "Codecademy",
+      "date" : 2012,
+      "url" : "http://www.codecademy.com/en/tracks/jquery"
+    },
+    {
+      "title" : "A Better Way to Learn AngularJS",
+      "school" : "Thinkster",
+      "date" : 2014,
+      "url" : "https://thinkster.io/angulartutorial/a-better-way-to-learn-angularjs/"
+    }
+  ]
+};
+
+// var formattedName = HTMLheaderName.replace("%data%", bio.name);
+// var formattedRole = HTMLheaderRole.replace("%data%", bio.role);
+// $("#header").append(formattedName).append(formattedRole);
